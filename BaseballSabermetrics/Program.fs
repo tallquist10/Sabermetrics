5311d078f6ca5416206a7beec747f41f5cb862d0
Caleb Tallquist <caleb.tallquist@hope.edu>
2020-08-09 20:46:04 -0400

    Added console feedback

--- BaseballSabermetrics/Program.fs
+++ BaseballSabermetrics/Program.fs
@@ -17,5 +17,7 @@ let getStatsForSabermetrics (players:Result<Hitter,string> []) =
        | _ -> () 
 [<EntryPoint>]
 let main argv =
+    printfn "Retrieving Baseball stats from baseball-reference.com. This will likely take 2+ hours..."
     getStatsForSabermetrics (getStatsForPlayers (getAllPlayers [|'a'..'z'|]))
+    printfn "Process complete. You may now exit this window."
     0 // return an integer exit code
