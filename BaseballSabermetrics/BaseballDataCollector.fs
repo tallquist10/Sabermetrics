b81129c8b729ce8156374c4f958fa2c82f66e2d2
Caleb Tallquist <caleb.tallquist@hope.edu>
2021-05-02 20:47:59 -0400

    Foundation functions for getting all stats for players. Gets up to the links to each player

--- BaseballSabermetrics/BaseballDataCollector.fs
+++ BaseballSabermetrics/BaseballDataCollector.fs
@@ -13,73 +13,90 @@ module BaseballDataCollector =
 
     module NewAPI =
         open Sabermetrics.Dependencies.NewHtmlHandler
-        let tryGetPlayerPage url (playerId:string) = 
-            getSite (sprintf "%s/%c/%s.shtml" url (playerId.ToCharArray().[0]) playerId)
+        module SinglePlayer =
+            let tryGetPlayerPage url (playerId:string) = 
+                getSite (sprintf "%s/%c/%s.shtml" url (playerId.ToCharArray().[0]) playerId)
 
-        let tryGetPlayerName playerId (doc:HtmlDocument) =
-            let nodes = tryGetHtmlSections "#info #meta h1 > span" doc
-            match nodes with
-            | Result.Ok []-> Result.Error (FailedWebResponseParseException (sprintf "Could not find the name for the player with ID %s" playerId))
-            | Result.Error e -> Result.Error e
-            | Result.Ok (head::_) -> Result.Ok(doc,head)
-
-        let tryCreatePlayer playerId (doc:HtmlDocument,nameElement:HtmlNode) = 
-            let name = HtmlNodeExtensions.InnerText nameElement
-            let stats = doc |> tryGetHtmlSections "#pitching_standard"
-            match stats with
-            | Result.Ok x -> Result.Ok (createPitcher name playerId)
-            | Result.Error _ ->
-                let stats = doc |> tryGetHtmlSections "#batting_standard" 
-                match stats with
-                | Result.Ok _ -> Result.Ok (createHitter name playerId)
-                | Result.Error _ -> Result.Error (FailedDomainTranslationException (sprintf "Could not create player %s" name))
-
-        let tryGatherPlayerStats doc player = 
-            match player with
-            | Hitter h -> doc |> (tryGetHtmlSections "#batting_standard tfoot tr")
-            | Pitcher p -> doc |> (tryGetHtmlSections "#pitching_standard tfoot tr")
-
-        let tryGetPlayerStats (doc:HtmlDocument) datasource player =
-            let updateStatsForPlayer (stats: string list) player =
-                match player with
-                | Hitter _ -> Result.Ok (updateStats stats datasource player)
-                | Pitcher _ -> Result.Ok (updateStats [] datasource player)
-
-            let playerWithStats = 
-                let statResult = tryGatherPlayerStats doc player
-                match statResult with
-                | Result.Ok statRow ->
-                    let careerRow = statRow |> List.head
-                    let careerStats = careerRow |> (fun n -> CssSelectorExtensions.CssSelect (n,"td"))
-                    let careerNumbers = careerStats |> List.map (fun html -> 
-                        HtmlNodeExtensions.InnerText html)
-                    let convertedStats = careerNumbers
-                    updateStatsForPlayer convertedStats player
-                    
+            let tryGetPlayerName playerId (doc:HtmlDocument) =
+                let nodes = tryGetHtmlSections "#info #meta h1 > span" doc
+                match nodes with
+                | Result.Ok []-> Result.Error (FailedWebResponseParseException (sprintf "Could not find the name for the player with ID %s" playerId))
                 | Result.Error e -> Result.Error e
-            playerWithStats
+                | Result.Ok (head::_) -> Result.Ok(doc,head)
 
-        let tryInsertPlayerIntoDatabase (da:IPlayerDataAccess) player =
-            da.InsertPlayerStats player
-            |> Result.bind (fun _ -> Result.Ok player)
+            let tryCreatePlayer playerId (doc:HtmlDocument,nameElement:HtmlNode) = 
+                let name = HtmlNodeExtensions.InnerText nameElement
+                let stats = doc |> tryGetHtmlSections "#pitching_standard"
+                match stats with
+                | Result.Ok x -> Result.Ok (createPitcher name playerId)
+                | Result.Error _ ->
+                    let stats = doc |> tryGetHtmlSections "#batting_standard" 
+                    match stats with
+                    | Result.Ok _ -> Result.Ok (createHitter name playerId)
+                    | Result.Error _ -> Result.Error (FailedDomainTranslationException (sprintf "Could not create player %s" name))
 
-        let tryGetPlayerStatsFromDatabase (da:IPlayerDataAccess) player =
-            match player with
-            | Hitter hitter -> da.GetStatsForHitter (PlayerID hitter.ID)
-            | Pitcher pitcher -> da.GetStatsForPitcher (PlayerID pitcher.ID)
+            let tryGatherPlayerStats doc player = 
+                match player with
+                | Hitter h -> doc |> (tryGetHtmlSections "#batting_standard tfoot tr")
+                | Pitcher p -> doc |> (tryGetHtmlSections "#pitching_standard tfoot tr")
 
-        let getPlayerStats url da datasource playerId =
-            let doc = tryGetPlayerPage url playerId
-            match doc with
-            | Result.Ok webpage ->
-                webpage
-                |> tryGetPlayerName playerId
-                |> Result.bind (tryCreatePlayer playerId)
-                |> Result.bind (tryGetPlayerStats webpage datasource)
-                |> Result.bind (tryInsertPlayerIntoDatabase da)
-                |> Result.bind (tryGetPlayerStatsFromDatabase da)
-            | Result.Error e -> Result.Error e
+            let tryGetPlayerStats (doc:HtmlDocument) datasource player =
+                let updateStatsForPlayer (stats: string list) player =
+                    match player with
+                    | Hitter _ -> Result.Ok (updateStats stats datasource player)
+                    | Pitcher _ -> Result.Ok (updateStats [] datasource player)
 
+                let playerWithStats = 
+                    let statResult = tryGatherPlayerStats doc player
+                    match statResult with
+                    | Result.Ok statRow ->
+                        let careerRow = statRow |> List.head
+                        let careerStats = careerRow |> (fun n -> CssSelectorExtensions.CssSelect (n,"td"))
+                        let careerNumbers = careerStats |> List.map (fun html -> 
+                            HtmlNodeExtensions.InnerText html)
+                        let convertedStats = careerNumbers
+                        updateStatsForPlayer convertedStats player
+                    
+                    | Result.Error e -> Result.Error e
+                playerWithStats
+
+            let tryInsertPlayerIntoDatabase (da:IPlayerDataAccess) player =
+                da.InsertPlayerStats player
+                |> Result.bind (fun _ -> Result.Ok player)
+
+            let tryGetPlayerStatsFromDatabase (da:IPlayerDataAccess) player =
+                match player with
+                | Hitter hitter -> da.GetStatsForHitter (PlayerID hitter.ID)
+                | Pitcher pitcher -> da.GetStatsForPitcher (PlayerID pitcher.ID)
+
+            let getPlayerStats url da datasource playerId =
+                let doc = tryGetPlayerPage url playerId
+                match doc with
+                | Result.Ok webpage ->
+                    webpage
+                    |> tryGetPlayerName playerId
+                    |> Result.bind (tryCreatePlayer playerId)
+                    |> Result.bind (tryGetPlayerStats webpage datasource)
+                    |> Result.bind (tryInsertPlayerIntoDatabase da)
+                    |> Result.bind (tryGetPlayerStatsFromDatabase da)
+                | Result.Error e -> Result.Error e
+        
+        module MultiplePlayers =
+            let tryGetLetterPage url letter =
+                try
+                    let players = getSite (sprintf "%s/players/%c/" url letter)
+                    match players with
+                    | Result.Ok html -> Result.Ok {Html = html; Letter = letter}
+                    | Result.Error _ -> Result.Error (FailedWebRequestException (sprintf "Could not load the webpage for players whose last names start with '%c'" (Char.ToUpper letter)))
+                with
+                | ex -> Result.Error (FailedWebRequestException (sprintf "Could not load the webpage for players whose last names start with '%c'" (Char.ToUpper letter)))
+            
+            let tryGetPlayersForLetter letterPage =
+                try
+                    let playerList = letterPage.Html |> Result.Ok |> getHtmlSections "#div_players_ p a"
+                    playerList
+                with
+                | e -> Result.Error (FailedWebRequestException (sprintf "Failed to extract list of players for letter '%c'" (Char.ToUpper letterPage.Letter)))
 
     module OldAPI =
         let getPlayerPage url player =
